821adcd9916cdf42e1437e0768fe6778edddb067
erobus <alikemof@gmail.com>
2020-12-20 16:23:18 +0300

    Initial Commit

--- config/server.js
+++ config/server.js
@@ -1,8 +1,4 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
- admin: {
-    url: '/', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
-    serveAdminPanel: true, // http://yourbackend.com will not serve any static admin files
-  },
 });
